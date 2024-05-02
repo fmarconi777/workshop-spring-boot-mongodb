e1ce911d6dd6b3b1ea39e8b8354b86ad879cda9d
Felipe Pedrosa <marconizero@gmail.com>
2024-05-02 17:11:11 -0300

    feat: create user entity

---
+++ src/main/java/com/workshop/workshopspringbootmongodb/domain/User.java
@@ -0,0 +1,52 @@
+package com.workshop.workshopspringbootmongodb.domain;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }    
+}
