09f9d64341f257a38ac5da81c948f72d8d6e257d
Felipe Pedrosa <marconizero@gmail.com>
2024-05-03 15:44:28 -0300

    refactor: findAll method returns the UserDTO instead of a User model

--- src/main/java/com/workshop/workshopspringbootmongodb/controller/UserController.java
+++ src/main/java/com/workshop/workshopspringbootmongodb/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.workshop.workshopspringbootmongodb.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.workshop.workshopspringbootmongodb.domain.user.User;
+import com.workshop.workshopspringbootmongodb.dto.UserDTO;
 import com.workshop.workshopspringbootmongodb.services.UserService;
 
 @RestController
@@ -19,8 +20,9 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        List<User> list = userService.findAll();
-        return ResponseEntity.ok(list);
+    public ResponseEntity<List<UserDTO>> findAll() {
+        var list = userService.findAll();
+        var listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+        return ResponseEntity.ok(listDTO);
     }
 }
