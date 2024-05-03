3d00b22f16d510db28b6eafd76773815f19599e9
Felipe Pedrosa <marconizero@gmail.com>
2024-05-03 17:13:28 -0300

    feat: add findById method on UserService

--- src/main/java/com/workshop/workshopspringbootmongodb/services/UserService.java
+++ src/main/java/com/workshop/workshopspringbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.workshop.workshopspringbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.workshopspringbootmongodb.domain.user.User;
 import com.workshop.workshopspringbootmongodb.repository.UserRepository;
+import com.workshop.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,16 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+        if (!user.isPresent()) {
+            throw new ObjectNotFoundException("Object not found");
+        }
+        return user.get();
+        
+        // Outra solução para o findById
+        // return user.orElseThrow(() -> new ObjectNotFoundException("Object not found")); //NOSONAR
     }
 }
