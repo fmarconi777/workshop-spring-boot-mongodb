2d2619b83cf842decaa832559e26563959e35426
Felipe Pedrosa <marconizero@gmail.com>
2024-05-10 09:25:35 -0300

    feat: add insert method POST on UserController

--- src/main/java/com/workshop/workshopspringbootmongodb/controller/UserController.java
+++ src/main/java/com/workshop/workshopspringbootmongodb/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.workshop.workshopspringbootmongodb.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.workshop.workshopspringbootmongodb.dto.UserDTO;
 import com.workshop.workshopspringbootmongodb.services.UserService;
@@ -31,5 +35,13 @@ public class UserController {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         var user = userService.findById(id);
         return ResponseEntity.ok(new UserDTO(user));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        var user = userService.fromDTO(objDto);
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
