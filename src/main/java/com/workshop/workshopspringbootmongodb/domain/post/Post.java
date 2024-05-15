d5b68f25765cd3df7c643e8b83157caf6ecee30c
Felipe <marconizero@gmail.com>
2024-05-15 09:15:38 -0300

    feat: create post entity

---
+++ src/main/java/com/workshop/workshopspringbootmongodb/domain/post/Post.java
@@ -0,0 +1,55 @@
+package com.workshop.workshopspringbootmongodb.domain.post;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.workshop.workshopspringbootmongodb.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document()
+public class Post implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Post other = (Post) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
