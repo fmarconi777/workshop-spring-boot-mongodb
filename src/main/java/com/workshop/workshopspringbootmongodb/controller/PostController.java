a74ee6823959413271ce7beeba0169931703c348
Felipe <marconizero@gmail.com>
2024-05-16 16:15:43 -0300

    feat: create PostController and method findById

---
+++ src/main/java/com/workshop/workshopspringbootmongodb/controller/PostController.java
@@ -0,0 +1,25 @@
+package com.workshop.workshopspringbootmongodb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.workshop.workshopspringbootmongodb.domain.post.Post;
+import com.workshop.workshopspringbootmongodb.services.PostService;
+
+@RestController
+@RequestMapping("/user")
+public class PostController {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        var post = service.findById(id);
+        return ResponseEntity.ok(post);
+    }
+}
